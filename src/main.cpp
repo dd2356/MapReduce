0e7080ec12e2db030265ed3aea91c6b629554620
Max <max.bergmark@gmail.com>
2019-05-22 11:59:30 +0200

    Added main script

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+
+
+int main(int argc, char **argv) {
+
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	MPI_File_open(MPI_COMM_WORLD, "../wiki_100k.txt", 
+		MPI_MODE_RDONLY, MPI_INFO_NULL, &fh );
+
+}
