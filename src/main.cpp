699d4d39cf31ed1b82f7a4159938e71f19f4e5a3
Max <max.bergmark@gmail.com>
2019-05-30 21:12:08 +0200

    More logging

--- src/main.cpp
+++ src/main.cpp
@@ -161,6 +161,7 @@ void mapreduce(int loop_limit, int rank, int size, MPI_File fh, char **buf,
 			reduce(receive_buffer[last_2], buff_sizes[last_2], process_map);
 			end = clock(); times[5] += ((double) (end - start)) / CLOCKS_PER_SEC;
 		}
+		printf("reached barrier on %d\n", rank);
 		MPI_Barrier(MPI_COMM_WORLD);
 		if (rank == 0) {
 			printf("\n");
