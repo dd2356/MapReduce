851a3c942d80c171b60e8dd52e9a61718dcb5dfb
Kristian Alvarez Jörgensen <krijor@kth.se>
2019-05-30 15:03:25 +0200

    Gather data to rank 0 at the end of main

--- include/msr_comm.h
+++ include/msr_comm.h
@@ -7,4 +7,8 @@
 
 void read(MPI_File *fh, char *buf, MPI_Offset chunk_size, 
 	MPI_Offset overlap, int iteration, int rank, int size, MPI_Offset file_size);
+
+void displacement(int *src, int *dst, int size);
+void define_pair_type(MPI_Datatype *type);
+int sum(int *arr, int size);
 int communicate(Pair *sendbuf, int *sendcounts, Pair **recvbuf, int n);
